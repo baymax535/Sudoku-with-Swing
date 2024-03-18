bcd3a74cd06a49a076d623b70ac103ec72d825eb
Labib Alam <labibalam1100@gmail.com>
2024-03-18 00:25:31 -0500

    Grid on Swing done

--- Sudoku/src/Grid.java
+++ Sudoku/src/Grid.java
@@ -1,6 +1,25 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Grid extends JFrame{
-		
+    private JTextField[][] grid;
+    
+	public Grid() {
+        setTitle("Sudoku Game");
+        setSize(400, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new GridLayout(4, 4));
+
+        grid = new JTextField[4][4];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                grid[i][j] = new JTextField();
+                grid[i][j].setHorizontalAlignment(JTextField.CENTER);
+                add(grid[i][j]);
+            }
+        setVisible(true);
+        }
+	}
 	
 }
--- Sudoku/src/Main.java
+++ Sudoku/src/Main.java
@@ -1,20 +1,26 @@
+import javax.swing.*;
 
 public class Main {
 
 	public static void main (String[] args) {
-		int aa=0,ab=0,ac=0,
-			ba=0,bb=0,bc=0,
-			ca=0,cb=0,cc=0;
+		int aa=1,ab=2,ac=3,ad=4,
+			ba=5,bb=6,bc=7,bd=8,
+			ca=9,cb=10,cc=11,cd=12,
+			da=13,db=14,dc=15,dd=16;
 		
 		int [][] board = {
-						  {aa},{ab},{ac},
-						  {ba},{bb},{bc},
-						  {ca},{cb},{cc}
+						  {aa},{ab},{ac},{ad},
+						  {ba},{bb},{bc},{bd},
+						  {ca},{cb},{cc},{cd},
+						  {da},{db},{dc},{dd}
 						 };
 		for (int i = 0; i<board.length; i++) {
 			System.out.println(board[i][0]);
 
 		}
+		SwingUtilities.invokeLater(() -> {
+            new Grid();
+        });
 	}
 	
 }
