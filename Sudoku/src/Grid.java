3ddd5679674a46394be9a64e5cb9240853560d7a
Labib Alam <labibalam1100@gmail.com>
2024-03-17 23:55:13 -0500

    Thinking about the logics as I go. Trying to draw board rn.

---
+++ Sudoku/src/Grid.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class Grid extends JFrame{
+		
+	
+}
--- Sudoku/src/Main.java
+++ Sudoku/src/Main.java
@@ -1,4 +1,20 @@
 
 public class Main {
 
+	public static void main (String[] args) {
+		int aa=0,ab=0,ac=0,
+			ba=0,bb=0,bc=0,
+			ca=0,cb=0,cc=0;
+		
+		int [][] board = {
+						  {aa},{ab},{ac},
+						  {ba},{bb},{bc},
+						  {ca},{cb},{cc}
+						 };
+		for (int i = 0; i<board.length; i++) {
+			System.out.println(board[i][0]);
+
+		}
+	}
+	
 }
