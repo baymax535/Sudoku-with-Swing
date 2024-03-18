2c5905e1ef4160994ca8f7cb69a795e37bb58555
Labib Alam <labibalam1100@gmail.com>
2024-03-18 06:11:32 -0500

    Next aim is to simplify Game class and make 9x9 board!

--- Sudoku/src/Main.java
+++ Sudoku/src/Main.java
@@ -31,6 +31,20 @@ public class Main {
 	public int[][] getBoard() {
 		return this.board;
 	}
+	public int[][] getMediumBoard() {
+		this.board = new int[][] {
+	         {5, 3, 0, 0, 7, 0, 0, 0, 0},
+	         {6, 0, 0, 1, 9, 5, 0, 0, 0},
+	         {0, 9, 8, 0, 0, 0, 0, 6, 0},
+	         {8, 0, 0, 0, 6, 0, 0, 0, 3},
+	         {4, 0, 0, 8, 0, 3, 0, 0, 1},
+	         {7, 0, 0, 0, 2, 0, 0, 0, 6},
+	         {0, 6, 0, 0, 0, 0, 2, 8, 0},
+	         {0, 0, 0, 4, 1, 9, 0, 0, 5},
+	         {0, 0, 0, 0, 8, 0, 0, 7, 9}
+	    };
+		return this.board;
+	}
 	//Setter for Board
 	public void setBoard() {
 		this.board = new int[][] {
--- Sudoku/src/Play.java
+++ Sudoku/src/Play.java
@@ -1,36 +1,46 @@
 import javax.swing.*;
+
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Play extends JFrame {
-	private JButton startButton;
+	private JButton startButton, startButton2;
     public Play() {
         setTitle("Sudoku Game Menu");
         setSize(400, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        setLayout(new GridLayout(2, 4));
      // Create Start button
-        startButton = new JButton("Start");
+        startButton = new JButton("Start Easy");
         startButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                startGame();
+                startGame(0);
             }
         });
         add(startButton);
+        
+     // Create Start button
+        startButton2 = new JButton("Start Medium");
+        startButton2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                startGame(1);
+            }
+        });
+        add(startButton2);
     }
 
-    private void startGame() {
-        // Start the Sudoku game by creating an instance of the Grid class
-        Main main = new Main();
+    private void startGame(int x) {
+    	if(x==0) {
+    	Main main = new Main();
         SwingUtilities.invokeLater(() -> {
             new Grid(main.getBoard());
         });
         main.Start();
-    }
+    	}
+    	else if(x==1) {
+            JOptionPane.showMessageDialog(Play.this, "Under Development, Try Easy Mode");
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new Play().setVisible(true);
-        });
+    	}
     }
 }
