a42a2ca86ffa8e1524d1f7b081c53a02414165cb
Labib Alam <labibalam1100@gmail.com>
2024-03-18 02:00:06 -0500

    Wrote setter method, hoping to use that in Swing for game!

--- Sudoku/src/Game.java
+++ Sudoku/src/Game.java
@@ -15,120 +15,123 @@ public class Game {
 		if(!validBox()) return false;
 		return true;
 	}
+	
 	public boolean validRow() {
 		//Row 1: 
-		if(main.getBox(1)==main.getBox(2)) return false;
-		if(main.getBox(1)==main.getBox(3)) return false;
-		if(main.getBox(1)==main.getBox(4)) return false;
-		if(main.getBox(2)==main.getBox(3)) return false;
-		if(main.getBox(2)==main.getBox(4)) return false;
-		if(main.getBox(3)==main.getBox(4)) return false;
+		if(main.getCell(1)==main.getCell(2)) return false;
+		if(main.getCell(1)==main.getCell(3)) return false;
+		if(main.getCell(1)==main.getCell(4)) return false;
+		if(main.getCell(2)==main.getCell(3)) return false;
+		if(main.getCell(2)==main.getCell(4)) return false;
+		if(main.getCell(3)==main.getCell(4)) return false;
 		//Row 2:
-		if(main.getBox(5)==main.getBox(6)) return false;
-		if(main.getBox(5)==main.getBox(7)) return false;
-		if(main.getBox(5)==main.getBox(8)) return false;
-		if(main.getBox(6)==main.getBox(7)) return false;
-		if(main.getBox(6)==main.getBox(8)) return false;
-		if(main.getBox(7)==main.getBox(8)) return false;
+		if(main.getCell(5)==main.getCell(6)) return false;
+		if(main.getCell(5)==main.getCell(7)) return false;
+		if(main.getCell(5)==main.getCell(8)) return false;
+		if(main.getCell(6)==main.getCell(7)) return false;
+		if(main.getCell(6)==main.getCell(8)) return false;
+		if(main.getCell(7)==main.getCell(8)) return false;
 		//Row 3:
-		if(main.getBox(9)==main.getBox(10)) return false;
-		if(main.getBox(9)==main.getBox(11)) return false;
-		if(main.getBox(9)==main.getBox(12)) return false;
-		if(main.getBox(10)==main.getBox(11)) return false;
-		if(main.getBox(10)==main.getBox(12)) return false;
-		if(main.getBox(11)==main.getBox(12)) return false;
+		if(main.getCell(9)==main.getCell(10)) return false;
+		if(main.getCell(9)==main.getCell(11)) return false;
+		if(main.getCell(9)==main.getCell(12)) return false;
+		if(main.getCell(10)==main.getCell(11)) return false;
+		if(main.getCell(10)==main.getCell(12)) return false;
+		if(main.getCell(11)==main.getCell(12)) return false;
 		//Row 4: 
-		if(main.getBox(13)==main.getBox(14)) return false;
-		if(main.getBox(13)==main.getBox(15)) return false;
-		if(main.getBox(13)==main.getBox(16)) return false;
-		if(main.getBox(14)==main.getBox(15)) return false;
-		if(main.getBox(14)==main.getBox(16)) return false;
-		if(main.getBox(15)==main.getBox(16)) return false;
+		if(main.getCell(13)==main.getCell(14)) return false;
+		if(main.getCell(13)==main.getCell(15)) return false;
+		if(main.getCell(13)==main.getCell(16)) return false;
+		if(main.getCell(14)==main.getCell(15)) return false;
+		if(main.getCell(14)==main.getCell(16)) return false;
+		if(main.getCell(15)==main.getCell(16)) return false;
 		return true;
 	}
 	public boolean validColumn() {
 		//Column 1: 
-		if(main.getBox(1)==main.getBox(5)) return false;
-		if(main.getBox(1)==main.getBox(9)) return false;
-		if(main.getBox(1)==main.getBox(13)) return false;
-		if(main.getBox(5)==main.getBox(9)) return false;
-		if(main.getBox(5)==main.getBox(13)) return false;
-		if(main.getBox(9)==main.getBox(13)) return false;
+		if(main.getCell(1)==main.getCell(5)) return false;
+		if(main.getCell(1)==main.getCell(9)) return false;
+		if(main.getCell(1)==main.getCell(13)) return false;
+		if(main.getCell(5)==main.getCell(9)) return false;
+		if(main.getCell(5)==main.getCell(13)) return false;
+		if(main.getCell(9)==main.getCell(13)) return false;
 		//Column 2:
-		if(main.getBox(2)==main.getBox(6)) return false;
-		if(main.getBox(2)==main.getBox(10)) return false;
-		if(main.getBox(2)==main.getBox(14)) return false;
-		if(main.getBox(6)==main.getBox(10)) return false;
-		if(main.getBox(6)==main.getBox(14)) return false;
-		if(main.getBox(10)==main.getBox(14)) return false;
+		if(main.getCell(2)==main.getCell(6)) return false;
+		if(main.getCell(2)==main.getCell(10)) return false;
+		if(main.getCell(2)==main.getCell(14)) return false;
+		if(main.getCell(6)==main.getCell(10)) return false;
+		if(main.getCell(6)==main.getCell(14)) return false;
+		if(main.getCell(10)==main.getCell(14)) return false;
 		//Column 3:
-		if(main.getBox(3)==main.getBox(7)) return false;
-		if(main.getBox(3)==main.getBox(11)) return false;
-		if(main.getBox(3)==main.getBox(15)) return false;
-		if(main.getBox(7)==main.getBox(11)) return false;
-		if(main.getBox(7)==main.getBox(15)) return false;
-		if(main.getBox(11)==main.getBox(15)) return false;
+		if(main.getCell(3)==main.getCell(7)) return false;
+		if(main.getCell(3)==main.getCell(11)) return false;
+		if(main.getCell(3)==main.getCell(15)) return false;
+		if(main.getCell(7)==main.getCell(11)) return false;
+		if(main.getCell(7)==main.getCell(15)) return false;
+		if(main.getCell(11)==main.getCell(15)) return false;
 		//Column 4: 
-		if(main.getBox(4)==main.getBox(8)) return false;
-		if(main.getBox(4)==main.getBox(12)) return false;
-		if(main.getBox(4)==main.getBox(16)) return false;
-		if(main.getBox(8)==main.getBox(12)) return false;
-		if(main.getBox(8)==main.getBox(16)) return false;
-		if(main.getBox(12)==main.getBox(16)) return false;
+		if(main.getCell(4)==main.getCell(8)) return false;
+		if(main.getCell(4)==main.getCell(12)) return false;
+		if(main.getCell(4)==main.getCell(16)) return false;
+		if(main.getCell(8)==main.getCell(12)) return false;
+		if(main.getCell(8)==main.getCell(16)) return false;
+		if(main.getCell(12)==main.getCell(16)) return false;
 
 		return true;
 	}
 	
 	public boolean validBox() {
 		//Box 1: 
-		if(main.getBox(1)==main.getBox(2)) return false;
-		if(main.getBox(1)==main.getBox(5)) return false;
-		if(main.getBox(1)==main.getBox(6)) return false;
-		if(main.getBox(2)==main.getBox(5)) return false;
-		if(main.getBox(2)==main.getBox(6)) return false;
-		if(main.getBox(5)==main.getBox(6)) return false;
+		if(main.getCell(1)==main.getCell(2)) return false;
+		if(main.getCell(1)==main.getCell(5)) return false;
+		if(main.getCell(1)==main.getCell(6)) return false;
+		if(main.getCell(2)==main.getCell(5)) return false;
+		if(main.getCell(2)==main.getCell(6)) return false;
+		if(main.getCell(5)==main.getCell(6)) return false;
 		//Box 2:
-		if(main.getBox(3)==main.getBox(4)) return false;
-		if(main.getBox(3)==main.getBox(7)) return false;
-		if(main.getBox(3)==main.getBox(8)) return false;
-		if(main.getBox(4)==main.getBox(7)) return false;
-		if(main.getBox(4)==main.getBox(8)) return false;
-		if(main.getBox(7)==main.getBox(8)) return false;
+		if(main.getCell(3)==main.getCell(4)) return false;
+		if(main.getCell(3)==main.getCell(7)) return false;
+		if(main.getCell(3)==main.getCell(8)) return false;
+		if(main.getCell(4)==main.getCell(7)) return false;
+		if(main.getCell(4)==main.getCell(8)) return false;
+		if(main.getCell(7)==main.getCell(8)) return false;
 		//Box 3:
-		if(main.getBox(9)==main.getBox(10)) return false;
-		if(main.getBox(9)==main.getBox(13)) return false;
-		if(main.getBox(9)==main.getBox(14)) return false;
-		if(main.getBox(10)==main.getBox(13)) return false;
-		if(main.getBox(10)==main.getBox(14)) return false;
-		if(main.getBox(13)==main.getBox(14)) return false;
+		if(main.getCell(9)==main.getCell(10)) return false;
+		if(main.getCell(9)==main.getCell(13)) return false;
+		if(main.getCell(9)==main.getCell(14)) return false;
+		if(main.getCell(10)==main.getCell(13)) return false;
+		if(main.getCell(10)==main.getCell(14)) return false;
+		if(main.getCell(13)==main.getCell(14)) return false;
 		//Box 4: 
-		if(main.getBox(11)==main.getBox(12)) return false;
-		if(main.getBox(11)==main.getBox(15)) return false;
-		if(main.getBox(11)==main.getBox(16)) return false;
-		if(main.getBox(12)==main.getBox(15)) return false;
-		if(main.getBox(12)==main.getBox(16)) return false;
-		if(main.getBox(15)==main.getBox(16)) return false;
+		if(main.getCell(11)==main.getCell(12)) return false;
+		if(main.getCell(11)==main.getCell(15)) return false;
+		if(main.getCell(11)==main.getCell(16)) return false;
+		if(main.getCell(12)==main.getCell(15)) return false;
+		if(main.getCell(12)==main.getCell(16)) return false;
+		if(main.getCell(15)==main.getCell(16)) return false;
 		return true;
 	}
 	
 	public boolean validValues() {
-		if (main.getBox(1) < 1 || main.getBox(1) > 4) return false;
-		if (main.getBox(2) < 1 || main.getBox(2) > 4) return false;
-		if (main.getBox(3) < 1 || main.getBox(3) > 4) return false;
-		if (main.getBox(4) < 1 || main.getBox(4) > 4) return false;
-		if (main.getBox(5) < 1 || main.getBox(5) > 4) return false;
-		if (main.getBox(6) < 1 || main.getBox(6) > 4) return false;
-		if (main.getBox(7) < 1 || main.getBox(7) > 4) return false;
-		if (main.getBox(8) < 1 || main.getBox(8) > 4) return false;
-		if (main.getBox(9) < 1 || main.getBox(9) > 4) return false;
-		if (main.getBox(10) < 1 || main.getBox(10) > 4) return false;
-		if (main.getBox(11) < 1 || main.getBox(11) > 4) return false;
-		if (main.getBox(12) < 1 || main.getBox(12) > 4) return false;
-		if (main.getBox(13) < 1 || main.getBox(13) > 4) return false;
-		if (main.getBox(14) < 1 || main.getBox(14) > 4) return false;
-		if (main.getBox(15) < 1 || main.getBox(15) > 4) return false;
-		if (main.getBox(16) < 1 || main.getBox(16) > 4) return false;
+		if (main.getCell(1) < 1 || main.getCell(1) > 4) return false;
+		if (main.getCell(2) < 1 || main.getCell(2) > 4) return false;
+		if (main.getCell(3) < 1 || main.getCell(3) > 4) return false;
+		if (main.getCell(4) < 1 || main.getCell(4) > 4) return false;
+		if (main.getCell(5) < 1 || main.getCell(5) > 4) return false;
+		if (main.getCell(6) < 1 || main.getCell(6) > 4) return false;
+		if (main.getCell(7) < 1 || main.getCell(7) > 4) return false;
+		if (main.getCell(8) < 1 || main.getCell(8) > 4) return false;
+		if (main.getCell(9) < 1 || main.getCell(9) > 4) return false;
+		if (main.getCell(10) < 1 || main.getCell(10) > 4) return false;
+		if (main.getCell(11) < 1 || main.getCell(11) > 4) return false;
+		if (main.getCell(12) < 1 || main.getCell(12) > 4) return false;
+		if (main.getCell(13) < 1 || main.getCell(13) > 4) return false;
+		if (main.getCell(14) < 1 || main.getCell(14) > 4) return false;
+		if (main.getCell(15) < 1 || main.getCell(15) > 4) return false;
+		if (main.getCell(16) < 1 || main.getCell(16) > 4) return false;
 		return true;
 	}
+	
+	
 		
 }
