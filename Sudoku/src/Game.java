a21025c8260cbf188d71816ec91ec1b124eab100
Labib Alam <labibalam1100@gmail.com>
2024-03-18 01:38:27 -0500

    Writing Skeleton Rules for 4x4 board

--- Sudoku/src/Game.java
+++ Sudoku/src/Game.java
@@ -1,10 +1,134 @@
 
 public class Game {
 	
-	Main board = new Main();
+	Main main = new Main();
+	int [][] board = main.getBoard();
 	
-	public void gameRules() {
-		
+	public boolean gameRules() {
+		//Values are between one and four
+		if(!validValues()) return false;
+		//Horizontally Different Values
+		if(!validRow()) return false;
+		//Vertically Different Values
+		if(!validColumn()) return false;
+		//Square Different Values
+		if(!validBox()) return false;
+		return true;
+	}
+	public boolean validRow() {
+		//Row 1: 
+		if(main.getBox(1)==main.getBox(2)) return false;
+		if(main.getBox(1)==main.getBox(3)) return false;
+		if(main.getBox(1)==main.getBox(4)) return false;
+		if(main.getBox(2)==main.getBox(3)) return false;
+		if(main.getBox(2)==main.getBox(4)) return false;
+		if(main.getBox(3)==main.getBox(4)) return false;
+		//Row 2:
+		if(main.getBox(5)==main.getBox(6)) return false;
+		if(main.getBox(5)==main.getBox(7)) return false;
+		if(main.getBox(5)==main.getBox(8)) return false;
+		if(main.getBox(6)==main.getBox(7)) return false;
+		if(main.getBox(6)==main.getBox(8)) return false;
+		if(main.getBox(7)==main.getBox(8)) return false;
+		//Row 3:
+		if(main.getBox(9)==main.getBox(10)) return false;
+		if(main.getBox(9)==main.getBox(11)) return false;
+		if(main.getBox(9)==main.getBox(12)) return false;
+		if(main.getBox(10)==main.getBox(11)) return false;
+		if(main.getBox(10)==main.getBox(12)) return false;
+		if(main.getBox(11)==main.getBox(12)) return false;
+		//Row 4: 
+		if(main.getBox(13)==main.getBox(14)) return false;
+		if(main.getBox(13)==main.getBox(15)) return false;
+		if(main.getBox(13)==main.getBox(16)) return false;
+		if(main.getBox(14)==main.getBox(15)) return false;
+		if(main.getBox(14)==main.getBox(16)) return false;
+		if(main.getBox(15)==main.getBox(16)) return false;
+		return true;
+	}
+	public boolean validColumn() {
+		//Column 1: 
+		if(main.getBox(1)==main.getBox(5)) return false;
+		if(main.getBox(1)==main.getBox(9)) return false;
+		if(main.getBox(1)==main.getBox(13)) return false;
+		if(main.getBox(5)==main.getBox(9)) return false;
+		if(main.getBox(5)==main.getBox(13)) return false;
+		if(main.getBox(9)==main.getBox(13)) return false;
+		//Column 2:
+		if(main.getBox(2)==main.getBox(6)) return false;
+		if(main.getBox(2)==main.getBox(10)) return false;
+		if(main.getBox(2)==main.getBox(14)) return false;
+		if(main.getBox(6)==main.getBox(10)) return false;
+		if(main.getBox(6)==main.getBox(14)) return false;
+		if(main.getBox(10)==main.getBox(14)) return false;
+		//Column 3:
+		if(main.getBox(3)==main.getBox(7)) return false;
+		if(main.getBox(3)==main.getBox(11)) return false;
+		if(main.getBox(3)==main.getBox(15)) return false;
+		if(main.getBox(7)==main.getBox(11)) return false;
+		if(main.getBox(7)==main.getBox(15)) return false;
+		if(main.getBox(11)==main.getBox(15)) return false;
+		//Column 4: 
+		if(main.getBox(4)==main.getBox(8)) return false;
+		if(main.getBox(4)==main.getBox(12)) return false;
+		if(main.getBox(4)==main.getBox(16)) return false;
+		if(main.getBox(8)==main.getBox(12)) return false;
+		if(main.getBox(8)==main.getBox(16)) return false;
+		if(main.getBox(12)==main.getBox(16)) return false;
+
+		return true;
 	}
 	
+	public boolean validBox() {
+		//Box 1: 
+		if(main.getBox(1)==main.getBox(2)) return false;
+		if(main.getBox(1)==main.getBox(5)) return false;
+		if(main.getBox(1)==main.getBox(6)) return false;
+		if(main.getBox(2)==main.getBox(5)) return false;
+		if(main.getBox(2)==main.getBox(6)) return false;
+		if(main.getBox(5)==main.getBox(6)) return false;
+		//Box 2:
+		if(main.getBox(3)==main.getBox(4)) return false;
+		if(main.getBox(3)==main.getBox(7)) return false;
+		if(main.getBox(3)==main.getBox(8)) return false;
+		if(main.getBox(4)==main.getBox(7)) return false;
+		if(main.getBox(4)==main.getBox(8)) return false;
+		if(main.getBox(7)==main.getBox(8)) return false;
+		//Box 3:
+		if(main.getBox(9)==main.getBox(10)) return false;
+		if(main.getBox(9)==main.getBox(13)) return false;
+		if(main.getBox(9)==main.getBox(14)) return false;
+		if(main.getBox(10)==main.getBox(13)) return false;
+		if(main.getBox(10)==main.getBox(14)) return false;
+		if(main.getBox(13)==main.getBox(14)) return false;
+		//Box 4: 
+		if(main.getBox(11)==main.getBox(12)) return false;
+		if(main.getBox(11)==main.getBox(15)) return false;
+		if(main.getBox(11)==main.getBox(16)) return false;
+		if(main.getBox(12)==main.getBox(15)) return false;
+		if(main.getBox(12)==main.getBox(16)) return false;
+		if(main.getBox(15)==main.getBox(16)) return false;
+		return true;
+	}
+	
+	public boolean validValues() {
+		if (main.getBox(1) < 1 || main.getBox(1) > 4) return false;
+		if (main.getBox(2) < 1 || main.getBox(2) > 4) return false;
+		if (main.getBox(3) < 1 || main.getBox(3) > 4) return false;
+		if (main.getBox(4) < 1 || main.getBox(4) > 4) return false;
+		if (main.getBox(5) < 1 || main.getBox(5) > 4) return false;
+		if (main.getBox(6) < 1 || main.getBox(6) > 4) return false;
+		if (main.getBox(7) < 1 || main.getBox(7) > 4) return false;
+		if (main.getBox(8) < 1 || main.getBox(8) > 4) return false;
+		if (main.getBox(9) < 1 || main.getBox(9) > 4) return false;
+		if (main.getBox(10) < 1 || main.getBox(10) > 4) return false;
+		if (main.getBox(11) < 1 || main.getBox(11) > 4) return false;
+		if (main.getBox(12) < 1 || main.getBox(12) > 4) return false;
+		if (main.getBox(13) < 1 || main.getBox(13) > 4) return false;
+		if (main.getBox(14) < 1 || main.getBox(14) > 4) return false;
+		if (main.getBox(15) < 1 || main.getBox(15) > 4) return false;
+		if (main.getBox(16) < 1 || main.getBox(16) > 4) return false;
+		return true;
+	}
+		
 }
--- Sudoku/src/Main.java
+++ Sudoku/src/Main.java
@@ -6,12 +6,18 @@ public class Main {
 	private int aa=1,ab=2,ac=3,ad=4,
 				ba=4,bb=3,bc=2,bd=1,
 				ca=3,cb=4,cc=1,cd=2,
-				da=2,db=1,dc=4,dd=3;	
+				da=2,db=1,dc=5,dd=3;	
 	public static void main (String[] args) {
 		Main main = new Main();
+		Game myGame = new Game();
 		SwingUtilities.invokeLater(() -> {
             new Grid(main.getBoard());
         });
+		
+		if(!myGame.gameRules()) {
+			System.out.println("Failed, Try Again!");
+		}
+		else System.out.println("You Won!");
 	}
 	
 	public Main(){
@@ -26,6 +32,13 @@ public class Main {
 	
 	public int[][] getBoard() {
 		return this.board;
+	}
+	public int getBox(int n) {
+		if(n==1) return aa;if(n==2) return ab;if(n==3) return ac;if(n==4) return ad;
+		if(n==5) return ba;if(n==6) return bb;if(n==7) return bc;if(n==8) return bd;
+		if(n==9) return ca;if(n==10) return cb;if(n==11) return cc;if(n==12) return cd;
+		if(n==13) return da;if(n==14) return db;if(n==15) return dc;if(n==16) return dd;
+		return 0;
 	}
 }
 
